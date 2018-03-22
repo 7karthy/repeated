6bc1eebcfd0dc89132566bbfc5abc44e18d6ec15
7karthy <36299114+7karthy@users.noreply.github.com>
2018-03-22 14:38:32 +0530

    Create composite.py

---
+++ composite.py
@@ -0,0 +1,10 @@
+k = int(input("Enter a number: "))
+if k > 1:
+   for i in range(2,k):
+       if (k % i) == 0:
+           print("yes")
+           break
+   else:
+       print("no")
+else:
+   print("yes")
